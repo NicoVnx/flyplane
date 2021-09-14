c129f8462cff2ef1b2e1b4f9ff43fcb1d1140cbd
nicov <nico.vnx07@gmail.com>
2021-09-14 13:09:57 -0300

    layouts

---
+++ public/script/card.js
@@ -0,0 +1,7 @@
+window.addEventListener("onmouseover", function(){
+
+    const cardc = document.querySelector(".card")
+    const card = document.querySelector(".cont-card")
+    card.classList.add("hide", onmouseover(cardc))
+
+})
